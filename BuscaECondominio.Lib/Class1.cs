3a44d7eb4716df0c3a7068b5dab16b20c30c2dae
Sidney <sidneymetalop@gmail.com>
2022-07-09 08:46:33 -0300

    Iniciando projeto DoDev

---
+++ BuscaECondominio.Lib/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace BuscaECondominio.Lib;
+public class Class1
+{
+
+}
---
+++ BuscaECondominio.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace BuscaECondominio.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
