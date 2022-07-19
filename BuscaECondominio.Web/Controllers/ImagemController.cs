ab37b11851378e5f032c2e809798fbec58ba16ed
sidney alexandre <sidneymetalop@gmail.com>
2022-07-19 15:47:49 -0300

    Ajustando HttpGet e HttpPost da classe ImagemController.

--- BuscaECondominio.Web/Controllers/ImagemController.cs
+++ BuscaECondominio.Web/Controllers/ImagemController.cs
@@ -11,14 +11,21 @@ namespace BuscaECondominio.Web.Controllers
     public class ImagemController : ControllerBase    
     {
         private readonly IAmazonS3 _amazonS3;
-        public ImagemController()
+        public ImagemController(IAmazonS3 amazonS3)
         {
-            
+            _amazonS3 = amazonS3;
         }
+        [HttpGet("bucket")]
+        public async Task<IActionResult> ListarBucket()
+        {
+            var resposta = await _amazonS3.ListBucketsAsync();
+            return Ok(resposta.Buckets.Select(x => x.BucketName));
+        }   
+
         [HttpPost("bucket")]
         public async Task<IActionResult> CriarBucket(string nomeBucket)
         {
-            var resposta = _amazonS3.PutBucketAsync(nomeBucket);
+            var resposta = await _amazonS3.PutBucketAsync(nomeBucket);
             return Ok(resposta);
         }    
     }
