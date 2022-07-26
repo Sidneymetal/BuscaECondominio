b8d8d169d64b21fbeebf077cf4d24fefa17efc91
sidney alexandre <sidneymetalop@gmail.com>
2022-07-26 18:29:36 -0300

    Testando APIs.

--- BuscaECondominio.Web/Controllers/UsuarioController.cs
+++ BuscaECondominio.Web/Controllers/UsuarioController.cs
@@ -20,22 +20,125 @@ namespace BuscaECondominio.Web.Controllers
             _application = application;
         }
         [HttpGet("ListarUsuario")]
+        // public async Task<IActionResult> ListarUsuarios()
+        // {
+        //     try
+        //     {
+        //         return Ok(await _application.ListarUsuarios());
+        //     }
+        //     catch (System.Exception)
+        //     {
+                
+        //         throw new Exception();
+        //     }
+        // }
         
         [HttpPost("AdicionarUsuario")]
+        public async Task<int> AdicionarUsuario(UsuarioDTO usuarioDTO)
+        {
+            try
+            {
+                return await _application.AdicionarUsuario(usuarioDTO);
+            }
+            catch (System.Exception)
+            {
+                
+                throw new Exception("Usuário não adicionado.");
+            }
+        }
         
         [HttpPost("CadastrarImagem")]
+        public async Task<bool> CadastrarImagem(int id, IFormFile imagem)
+        {
+            try
+            {
+                return await _application.CadastrarImagem(id, imagem);
+            }
+            catch (System.Exception)
+            {
+                
+                throw new Exception("Imagem não cadastrada.");
+            }
+        }
          
-        [HttpPost("ValidarImagem")]         
+        [HttpPost("ValidarImagem")] 
+        public async Task<bool> ValidarImagem(string nomeArquivo) 
+        {
+            try
+            {
+                return await _application.ValidarImagem(nomeArquivo);
+            }
+            catch (System.Exception)
+            {
+                
+                throw new Exception("Imagem não foi validada.");
+            }
+        }       
         
-        [HttpPost("LoginPorEmailESenha")]
+        [HttpGet("LoginPorEmailESenha")]
+        public async Task<bool> LoginPorEmailESenha(string email, string senha)
+        {
+            try
+            {
+                return await _application.LoginPorEmailESenha(email, senha);
+            }
+            catch (System.Exception)
+            {
+                
+                throw new Exception("Login não efetuado.");
+            }
+        }
 
-        [HttpPost("ConferirSenhaDoUsuario")]
+        // [HttpGet("ConferirSenhaDoUsuario")]
+        // public async Task<bool> ConferirSenhaDoUsuario(Usuario idUsuario, string senha)
+        // {
+        //     try
+        //     {
+        //         return await _application.ConferirSenhaDoUsuario(idUsuario, senha);
+        //     }
+        //     catch (System.Exception)
+        //     {
+                
+        //         throw new Exception("Senha inválida.")
+        //     }
+        // }
 
-        [HttpPost("BuscarUsuarioPorImagem")]
+        [HttpGet("BuscarUsuarioPorImagem")]
+        public async Task<bool> BuscarUsuarioPorImagem(string urlImagemCadastro, IFormFile image)
+        {
+            try
+            {
+                return await _application.BuscarUsuarioPorImagem(urlImagemCadastro, image);
+            }
+            catch (System.Exception)
+            {
+                
+                throw new Exception("Imagem não encontrada.");
+            }
+        }
 
-        [HttpPut("AlterarSenha")]        
+        [HttpPut("AlterarSenha")]  
+        public async Task<bool> AlterarSenha(int id, string alterarSenha) 
+        {
+            return await _application.AlterarSenha(id, alterarSenha);
+        }     
         
-        [HttpDelete("DeletarUsuario")]          
+        [HttpDelete("DeletarUsuario")]  
+        public async Task<bool> DeletarUsuario(int id)
+        {
+            try
+            {
+                return await _application.DeletarUsuario(id);
+            }
+            catch (System.Exception)
+            {
+                
+                throw new Exception ("Usuário não deletado.");
+            }
+        }       
     }
 }
 
+ 
+         
+         
